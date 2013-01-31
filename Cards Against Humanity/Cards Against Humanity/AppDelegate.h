57cf5f3f9342e231e61580a99e52c129663fdfc1
Kendra Cunningham <cunningkendra@gmail.com>
2013-01-30 17:28:31 -0700

    Updated default files

--- Cards Against Humanity/Cards Against Humanity/AppDelegate.h
+++ Cards Against Humanity/Cards Against Humanity/AppDelegate.h
@@ -6,21 +6,11 @@
 //  Copyright (c) 2013 Kendra Cunningham. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
-
 @class MainViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 @property (strong, nonatomic) MainViewController *viewController;
-
-
-@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
-@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
-@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
-
-- (void)saveContext;
-- (NSURL *)applicationDocumentsDirectory;
 
 @end
